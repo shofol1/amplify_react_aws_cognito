4b4985ab03d13de604466bebe0d63378a5489ae2
shofol <18103156@iubat.edu>
2022-03-02 17:14:53 +0600

    cognito_aws

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify, { Auth } from 'aws-amplify';
+import { AmplifySignOut,withAuthenticator } from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
+import '@aws-amplify/ui-react/styles.css';
+Amplify.configure(awsconfig);
 
-function App() {
+Amplify.configure(awsconfig)
+function App(signOut,user) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App-header'>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App) ;
